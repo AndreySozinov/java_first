eb902aced174c9f2820e445ffdb1fe1b3ca0a9d9
AndreySozinov <savrey@ya.ru>
2022-06-13 16:57:59 +0500

    second task began

--- project_one/program.java
+++ project_one/program.java
@@ -3,10 +3,8 @@ package project_one;
 /* 1. Задана натуральная степень k. Сформировать случайным образом список
  * коэффициентов (от 0 до 100) многочлена степени k.
  * Пример: k = 2 => 2*х^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*х^2 = 0
- * 
- * 2. Даны два файла, в каждом из которых находится запись многочлена.
- * Сформировать файл, содержащий сумму многочленов.
 */
+
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -14,14 +12,14 @@ public class program {
     public static void main(String[] args) {
         String polynomial = "";
         int k = Power_k(); // Вводим степень k.
-        int[] array_coeffs = Coeffs(k); // Получаем случайные коэффициенты.
+        int[] array_coeffs = Coeffs(k); // Получаем массив с случайными коэффициентами.
         for (int i = k; i > 0; i--) { // Собираем строку с многочленом.
-            polynomial = polynomial + Elements(array_coeffs[k-i], i);
+            polynomial = polynomial + Elements(array_coeffs[k-i], i); // Метод Elements выдает готовые элементы многочлена.
             if (Elements(array_coeffs[k-i], i) != "") polynomial = polynomial + " + ";
         }
         if (array_coeffs[k] == 0) polynomial = polynomial.substring(0, -1);
-        else polynomial = polynomial + array_coeffs[k];
-        polynomial = polynomial + " = 0";
+        else polynomial = polynomial + array_coeffs[k]; // Добавляем последний элемент.
+        polynomial = polynomial + " = 0"; // Дописываем окончание многочлена.
         System.out.println(polynomial);
     }
 
