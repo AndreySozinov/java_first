814b76063a0283da00ff530707aaa8c95ab72c59
AndreySozinov <savrey@ya.ru>
2022-07-09 19:24:10 +0500

    task 12 - правильность скобок в выражении

--- task10/program10.java
+++ task10/program10.java
@@ -1,37 +1,67 @@
 package task10;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 // Реализовать алгоритм перевода из инфиксной записи в постфиксную 
 // для арифметического выражения.
 // Вычислить запись если это возможно.
 public class program10 {
     public static void main(String[] args) {
-        String infix_expression = "25+9*5.0-100";
+        String infix_expression = "25+9*5.0/10-4";
         String postfix_expression = "";
 
-        Map<String, Integer> priority = new HashMap<String, Integer>();
-        priority.put("+", 1);
-        priority.put("-", 1);
-        priority.put("*", 2);
-        priority.put("/", 2);
-        priority.put("^", 3);
-
         LinkedList<String> elements = GetElements(infix_expression);
-        System.out.println(elements);
-        postfix_expression = Translate(elements, priority);
-        //System.out.println(postfix_expression);
+        postfix_expression = Translate(elements);
+        System.out.println(postfix_expression);
     }
 
-    static String Translate(LinkedList<String> enter, Map<String, Integer> priority) {
+    // Переводим строки чисел и операций в постфиксную запись с учетом приоритета.
+    static String Translate(LinkedList<String> enter) {
         String result = "";
-
+        StringBuilder numbers = new StringBuilder();
+        StringBuilder operations = new StringBuilder();
+        int position = -1;
+        while (enter.contains("^")) {
+            position = enter.indexOf("^");
+            Replace(position, enter, numbers, operations);
+        }
+        while (enter.contains("*") || enter.contains("/")) {
+            if (enter.contains("*") && enter.contains("/")) {
+                position = enter.indexOf("*") < enter.indexOf("/")? 
+                            enter.indexOf("*") : enter.indexOf("/"); 
+                Replace(position, enter, numbers, operations);
+            }
+            else if (enter.contains("*")) {
+                position = enter.indexOf("*");
+                Replace(position, enter, numbers, operations);
+            }
+            else {
+                position = enter.indexOf("/");
+                Replace(position, enter, numbers, operations);
+            }
+        }
+        while (enter.contains("+") || enter.contains("-")) {
+            if (enter.contains("+") && enter.contains("-")) {
+                position = enter.indexOf("+") < enter.indexOf("-")? 
+                            enter.indexOf("+") : enter.indexOf("-"); 
+                Replace(position, enter, numbers, operations);
+            }
+            else if (enter.contains("+")) {
+                position = enter.indexOf("+");
+                Replace(position, enter, numbers, operations);
+            }
+            else {
+                position = enter.indexOf("-");
+                Replace(position, enter, numbers, operations);
+            }
+        }
+        result = result.concat(numbers.toString());
+        result = result.concat(operations.toString());
+        result = result.replace(" 0", "");
         return result;
     }
 
-    // Разбиваем выражение на числа и знаки операций.
+    // Разбиваем строку с исходным выражением на список чисел и знаков операций.
     static LinkedList<String> GetElements (String source) {
         LinkedList<String> result = new LinkedList<>();
         int start = 0;
@@ -51,5 +81,18 @@ public class program10 {
         }
         result.add(source.substring(start));
         return result;
+    }
+
+    // Раскидываем список с числами и знаками на строчку чисел и строчку знаков.
+    static void Replace(int pos, LinkedList<String> source, StringBuilder nums, StringBuilder signs) {
+        nums.append(source.get(pos-1));
+        nums.append(" ");
+        nums.append(source.get(pos + 1));
+        nums.append(" ");
+        signs.append(source.get(pos));
+        signs.append(" ");
+        source.set(pos - 1, "0");
+        source.remove(pos);
+        source.remove(pos);
     }
 }
--- task12/program12.java
+++ task12/program12.java
@@ -7,8 +7,78 @@ package task12;
 // Пример 5: <{a}+{(d*3)}> - истина
 // Пример 6: {a+]}{(d*3)} - ложь
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class program12 {
     public static void main(String[] args) {
-        
+        String expression = "a+(d*3)";
+        System.out.println(Correct(expression));
+
+        expression = "[a+(1*3)";
+        System.out.println(Correct(expression));
+
+        expression = "[6+(3*3)]";
+        System.out.println(Correct(expression));
+
+        expression = "{a}[+]{(d*3)}";
+        System.out.println(Correct(expression));
+
+        expression = "<{a}+{(d*3)}>";
+        System.out.println(Correct(expression));
+
+        expression = "{a+]}{(d*3)}";
+        System.out.println(Correct(expression));
+    }
+
+    static boolean Correct(String str) {
+        Map<String, String> dict = new HashMap<>();
+        dict.put("(", ")");
+        dict.put("[", "]");
+        dict.put("{", "}");
+        dict.put("<", ">");
+        Stack<String> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            switch (str.charAt(i)) {
+                case '(': 
+                    st.push(dict.get("("));
+                    break;
+                case '[': 
+                    st.push(dict.get("["));
+                    break;
+                case '{': 
+                    st.push(dict.get("{"));
+                    break;
+                case '<': 
+                    st.push(dict.get("<"));
+                    break;
+                case ')': 
+                    if (st.peek().equals(Character.toString(str.charAt(i)))) {
+                        st.pop();
+                    }
+                    else {st.push(")");}
+                    break;
+                case ']': 
+                    if (st.peek().equals(Character.toString(str.charAt(i)))) {
+                        st.pop();
+                    }
+                    else {st.push("]");}
+                    break;
+                case '}': 
+                    if (st.peek().equals(Character.toString(str.charAt(i)))) {
+                        st.pop();
+                    }
+                    else {st.push("}");}
+                    break;
+                case '>': 
+                    if (st.peek().equals(Character.toString(str.charAt(i)))) {
+                        st.pop();
+                    }
+                    else {st.push(">");}
+                    break;
+            }
+        }
+        return st.empty();
     }
 }
