43bab24e3a81b58fabed3c72bf494f69cb21b3c7
AndreySozinov <savrey@ya.ru>
2022-07-13 20:14:28 +0500

    сделан алгоритм, переводящий римские числа в арабские

---
+++ task_rome/program_rome.java
@@ -0,0 +1,36 @@
+package task_rome;
+// Перевод римских чисел в арабские.
+import java.util.HashMap;
+import java.util.Map;
+
+public class program_rome {
+    public static void main(String[] args) {
+        Map<Character, Integer> numbers = new HashMap<>();
+        numbers.put('I', 1);
+        numbers.put('V', 5);
+        numbers.put('X', 10);
+        numbers.put('L', 50);
+        numbers.put('C', 100);
+        numbers.put('D', 500);
+        numbers.put('M', 1000);
+        
+        String roman_number = "MCMLXXVI";
+        int result = 0;
+        
+        int i = 1;
+
+        while (i < roman_number.length()) {
+            if (numbers.get(roman_number.charAt(i - 1)) < numbers.get(roman_number.charAt(i))) {
+                result = result - numbers.get(roman_number.charAt(i - 1));
+            }
+            else {
+            result = result + numbers.get(roman_number.charAt(i - 1));
+            }
+            i++;
+        }
+        result = result + numbers.get(roman_number.charAt(i-1));
+
+        System.out.printf("%s = %d", roman_number, result);
+    }
+    
+}
