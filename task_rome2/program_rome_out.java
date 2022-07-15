eeda66b50c3dad7f79be0f3c972fe691ac5011a6
AndreySozinov <savrey@ya.ru>
2022-07-15 17:36:49 +0500

    сделан алгоритм, переводящий арабские числа в римские с использованием TreeMap

--- task_rome2/program_rome_out.java
+++ task_rome2/program_rome_out.java
@@ -1,87 +1,46 @@
 package task_rome2;
 
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.TreeMap;
 
 public class program_rome_out {
+
+    public static TreeMap<Integer, Character> numbers = new TreeMap<>();
     public static void main(String[] args) {
-        // Map<Character, Integer> numbers = new HashMap<>();
-        // numbers.put('I', 1);
-        // numbers.put('V', 5);
-        // numbers.put('X', 10);
-        // numbers.put('L', 50);
-        // numbers.put('C', 100);
-        // numbers.put('D', 500);
-        // numbers.put('M', 1000);
+        
+        numbers.put(1, 'I');
+        numbers.put(5, 'V');
+        numbers.put(10, 'X');
+        numbers.put(50, 'L');
+        numbers.put(100, 'C');
+        numbers.put(500, 'D');
+        numbers.put(1000, 'M');
 
-        int arabic_number = 949;
-        System.out.print(arabic_number);
+        int arabic_number = 1949;
 
+        System.out.printf("%d = %s", arabic_number, getRoman(arabic_number));
+    }
+
+    static String getRoman(int arabic_number) {
         StringBuilder roman_number = new StringBuilder();
 
-        if (arabic_number / 1000 > 0) {
-            for (int i = 0; i < arabic_number / 1000; i++) {
-                roman_number.append("M");
-            }
-            arabic_number = arabic_number - 1000 * (arabic_number / 1000);
-        }
-        if (arabic_number / 500 > 0) {
-            for (int i = 0; i < arabic_number / 500; i++) {
-                roman_number.append("D");
-            }
-            arabic_number = arabic_number - 500 * (arabic_number / 500);
-        }
-        if (arabic_number / 100 > 0) {
-            for (int i = 0; i < arabic_number / 100; i++) {
-                roman_number.append("C");
-            }
-            arabic_number = arabic_number - 100 * (arabic_number / 100);
-        }
-        if (arabic_number / 50 > 0) {
-            for (int i = 0; i < arabic_number / 50; i++) {
-                roman_number.append("L");
-            }
-            arabic_number = arabic_number - 50 * (arabic_number / 50);
-        }
-        if (arabic_number / 10 > 0) {
-            for (int i = 0; i < arabic_number / 10; i++) {
-                roman_number.append("X");
-            }
-            arabic_number = arabic_number - 10 * (arabic_number / 10);
-        }
-        if (arabic_number / 5 > 0) {
-            for (int i = 0; i < arabic_number / 5; i++) {
-                roman_number.append("V");
-            }
-            arabic_number = arabic_number - 5 * (arabic_number / 5);
-        }
-        if (arabic_number  > 0) {
-            for (int i = 0; i < arabic_number; i++) {
-                roman_number.append("I");
+        for (int i : numbers.descendingKeySet()) {
+            if (arabic_number / i > 0) {
+                for (int j = 0; j < arabic_number / i; j++) {
+                    roman_number.append(numbers.get(i));
+                }
+                arabic_number = arabic_number % i;
             }
         }
         
         String number = roman_number.toString();
         
-        if (number.contains("DCCCC")) {
-            number = number.replace("DCCCC", "CM");
-            }
-        if (number.contains("CCCC")) {
-            number = number.replace("CCCC", "CD");
-            }
-        if (number.contains("LXXXX")) {
-            number = number.replace("LXXXX", "XC");
-            }
-        if (number.contains("XXXX")) {
-            number = number.replace("XXXX", "XL");
-            }
-        if (number.contains("VIIII")) {
-            number = number.replace("VIIII", "IX");
-            }
-        if (number.contains("IIII")) {
-            number = number.replace("IIII", "IV");
-            }
+        number = number.replace("DCCCC", "CM");                
+        number = number.replace("CCCC", "CD");        
+        number = number.replace("LXXXX", "XC");        
+        number = number.replace("XXXX", "XL");            
+        number = number.replace("VIIII", "IX");                
+        number = number.replace("IIII", "IV");
 
-        System.out.printf(" = %s", number);
+        return number;
     }
 }
